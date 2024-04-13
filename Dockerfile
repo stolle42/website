30e56951fd8a787732a91b948d1b332fae601a37
stolle42 <simon.stoll@arcor.de>
2024-04-13 11:11:13 +0200

    added some manifest files

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-#starting point:linux alpine with node installed
-FROM node:alpine 
-#simple app: container only needs one file
-COPY script.js /home 
-#run the script, so we can see the output
-CMD node /home/script.js 
+#1: starting point:linux alpine with nginx installed
+FROM nginx:alpine 
+#2: copy our website to the place nginx expects it
+COPY index.html /usr/share/nginx/html/
+#3: expose Port 80
+EXPOSE 80
--- helloreact/src/App.js
+++ helloreact/src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
+import './App.css'; //import styling
+import Navbar from './Navbar';
+import SimpleComponent from './buttonComponent';
+import opa from './miladsopa.png';
 
-function App() {
+function App() //java script function defining a component
+{
+  //here you can add java script code if you need it
   var rand=Math.random()*100
   return (
     <div className="App">
-      <div className="content">
+      <Navbar/>
+      <div>
         <h1>Hello world</h1>
         <p>Random number: {rand}</p>
+        <img src={opa} alt="nothing" />
       </div>
+      <SimpleComponent/>
+      <SimpleComponent/>
     </div>
   );
 }
-
-export default App;
+export default App;//export function, so react can use it
